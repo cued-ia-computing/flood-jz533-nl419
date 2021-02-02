aef2e17acd5e30b340b07e2d7f41a26b0e511bbf
Jinxuan Zhang <jz533@cam.ac.uk>
2021-02-02 20:21:19 +0000

    Complete Task 2C

--- floodsystem/flood.py
+++ floodsystem/flood.py
@@ -18,3 +18,21 @@ def stations_level_over_threshold(stations, tol):
         else:
             pass
     return sorted_by_key(list_of_stations, 1, True)
+
+
+def stations_highest_rel_level(stations, N):
+    """A function that returns a list of the N stations at which the
+    relative water level is highest. List should be sorted in descending
+    order."""
+    list_of_stations_have_rel = []
+    for i in stations:
+        if MonitoringStation.relative_water_level(i) is None:
+            pass
+        else:
+            one_tuple2 = (i, MonitoringStation.relative_water_level(i))
+            list_of_stations_have_rel.append(one_tuple2)
+    sorted_list = sorted_by_key(list_of_stations_have_rel, 1, True)
+    final_total_list = []
+    for i in sorted_list:
+        final_total_list.append(i[0])
+    return final_total_list[:N]
