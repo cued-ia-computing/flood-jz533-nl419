c40689f82b33d00971ec6b14306c84ba2b2d0b94
Jinxuan Zhang <jz533@cam.ac.uk>
2021-02-09 22:41:29 +0000

    Modified unit test for geo

--- test_geo.py
+++ test_geo.py
@@ -23,6 +23,6 @@ def test_stations_within_radius():
     s1 = MonitoringStation("s_id1", "m_id1", "A station", (0.0, 1.0), (1.0, 2.0), "r1", "t1")
     s2 = MonitoringStation("s_id2", "m_id2", "B station", (0.0, 3.0), (0.0, 2.0), "r2", "t2")
     stations = [s1, s2]
-    station_within_radius = stations_within_radius(stations, (0.0), 2)
+    station_within_radius = stations_within_radius(stations, (0.0, 0.0), 2)
 
     assert station_within_radius == [s1]
