8117f9093e8ad1bef60f7d36466443659f63d3a3
Jinxuan Zhang <jz533@cam.ac.uk>
2021-02-09 23:36:34 +0000

    Add unit test for geo

--- test_geo.py
+++ test_geo.py
@@ -4,6 +4,7 @@ from floodsystem.geo import stations_by_distance
 from floodsystem.station import MonitoringStation
 from haversine import haversine
 from floodsystem.geo import stations_within_radius
+from floodsystem.geo import rivers_with_station
 
 
 def test_stations_by_distance():
@@ -26,3 +27,14 @@ def test_stations_within_radius():
     stations = [s1, s2, s3]
 
     assert stations_within_radius(stations, (1.0, 1.0), 300) == [s1, s3]
+
+
+def test_rivers_with_station():
+
+    # Create stations
+    s1 = MonitoringStation("s_id1", "m_id1", "A station", (1.0, 2.0), (1.0, 2.0), "r1", "t1")
+    s2 = MonitoringStation("s_id2", "m_id2", "B station", (1.0, 4.0), (0.0, 2.0), "r2", "t2")
+    s3 = MonitoringStation("s_id3", "m_id3", "C station", (1.0, 3.0), (3.0, 2.0), "r2", "t3")
+    stations = [s1, s2, s3]
+
+    assert rivers_with_station(stations) == {'r2', 'r1'}
