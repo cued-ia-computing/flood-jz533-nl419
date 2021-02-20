b89be307ff02c91c40133bcdd9fe868ca68e9b3e
Nik Lebedenko <nl419@cam.ac.uk>
2021-02-20 11:39:46 +0000

    Added option to plot forecasted water levels

--- floodsystem/plot.py
+++ floodsystem/plot.py
@@ -1,6 +1,8 @@
-import matplotlib.dates
+import matplotlib.dates as matdate
 import matplotlib.pyplot as plt
 from .analysis import polyfit
+import numpy as np
+import datetime
 
 
 def plot_water_levels(station, dates, levels):
@@ -23,9 +25,13 @@ def plot_water_levels(station, dates, levels):
     plt.show()
 
 
-def plot_water_level_with_fit(station, dates, levels, p):
+def plot_water_level_with_fit(station, dates, levels, p, forecast=0):
+    """Plots the water level against date, including a least-squares regression
+    polynomial of order p and the typical range for the station. Also can
+    plot the polynomial for an additional 'forecast' days ahead. Note that
+    the forecast will usually be very obviously wrong."""
     poly, d0 = polyfit(dates, levels, p)
-    numericalDates = matplotlib.dates.date2num(dates)
+    numericalDates = matdate.date2num(dates)
 
     plt.plot(dates, poly(numericalDates - d0))
     plt.plot(dates, levels)
@@ -35,6 +41,13 @@ def plot_water_level_with_fit(station, dates, levels, p):
     plt.xticks(rotation=45)
     plt.axhline(y=station.typical_range[0])
     plt.axhline(y=station.typical_range[1])
+
+    if forecast >= 1:
+        forecast = int(forecast)
+        forecastStart = matdate.date2num(dates[0])
+        forecastEnd = matdate.date2num(dates[0] + datetime.timedelta(days=forecast))
+        forecastRange = np.linspace(forecastStart, forecastEnd, 10)
+        plt.plot(matdate.num2date(forecastRange), poly(forecastRange - d0))
 
     plt.tight_layout()
     plt.show()
